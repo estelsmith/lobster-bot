736a283a333e81ab0064e29c3331fd4d43f8ccff
Estel Smith <estel.smith@gmail.com>
2025-07-05 18:25:52 -0400

    stuff

--- app/config.py
+++ app/config.py
@@ -1,10 +1,40 @@
+from dataclasses import dataclass
 import functools
+import os
 
-from dotenv import load_dotenv
+from dotenv import dotenv_values, load_dotenv
+
+@dataclass
+class AppConfig:
+    """Core application configuration"""
+
+    APP_DATA_DIRECTORY: str = f'{os.getcwd()}/data'
+    """Directory where data files will be written."""
+
+    PAGES_REPOSITORY_FILE: str = '/pages.db'
+    """File where cached pages will be stored."""
+
+    OPENAI_COMPLETIONS_ENDPOINT: str = 'http://localhost:11434/v1/chat/completions'
+    """
+    URL to an OpenAI-compatible Chat Completions endpoint
+    
+    Defaults to local Ollama endpoint.
+    @see https://ollama.com/blog/openai-compatibility
+    """
+
+    OPENAI_MODEL: str = 'gemma3:27b'
+    """
+    The LLM model to use when handling anything
+    """
 
 @functools.cache
-def load_config():
-    """
-    Loads the application configuration a single time.
-    """
-    load_dotenv()
+def get_config() -> AppConfig:
+    """Loads application configuration a single time."""
+    config = AppConfig(**dotenv_values())
+
+    try:
+        os.mkdir(config.APP_DATA_DIRECTORY)
+    except:
+        pass
+
+    return config
--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 import inspect
 import sys
 
+import duckdb
 import fire
 import httpx
 
-from app.HttpxPageFetcher import HttpxPageFetcher
-from app.config import load_config
+from app.config import get_config
+from app.pages import DuckDbPageRepository
+from app.scraping import HttpxPageFetcher
 
-async def scrape():
+def scrape():
     """
     Scrape a URL and clean its content for analyzing with an LLM.
     """
-    client = httpx.AsyncClient()
+    client = httpx.Client()
     fetcher = HttpxPageFetcher(client)
-    response = await fetcher.fetch_page('https://news.ycombinator.com/')
+    response = fetcher.fetch_page('https://news.ycombinator.com/')
     print(f'{response!r}')
 
 def test():
-    pass
+    config = get_config()
+    connection = duckdb.connect(f'{config.APP_DATA_DIRECTORY}/{config.PAGES_REPOSITORY_FILE}')
+    repository = DuckDbPageRepository(connection)
 
 if __name__ == '__main__':
-    load_config()
+    get_config()
 
     # Available commands are every function defined in this particular module
-    commands = [obj for i,obj in inspect.getmembers(sys.modules[__name__]) if inspect.isfunction(obj)]
+    commands = [obj for i, obj in inspect.getmembers(sys.modules[__name__]) if inspect.isfunction(obj)]
 
     # Fire expects a dictionary of command_name => function, so create a dict where the key is the function's name
     command_obj = {i.__name__: i for i in commands}
