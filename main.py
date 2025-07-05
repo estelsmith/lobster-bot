7ca1a6131daf608b9cc62674ecd3e971ee553f6f
Estel Smith <estel.smith@gmail.com>
2025-07-04 22:09:11 -0400

    simple test

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import fire
+import httpx
 
-def scrape():
+from app.HttpxPageFetcher import HttpxPageFetcher
+
+async def scrape():
     """
     Scrape a URL and clean its content for analyzing with an LLM.
     """
-    print('Hello world!')
+    client = httpx.AsyncClient()
+    fetcher = HttpxPageFetcher(client)
+    response = await fetcher.fetch_page('https://news.ycombinator.com/')
+    print(f'{response!r}')
 
 if __name__ == '__main__':
     fire.Fire({'scrape': scrape})
