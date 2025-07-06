e987c898f0ed8a7735476bcc2a12bc56bbaf1d31
Estel Smith <estel.smith@gmail.com>
2025-07-06 16:30:57 -0400

    annotations

--- app/ioc/__init__.py
+++ app/ioc/__init__.py
@@ -1,3 +1,5 @@
 from .ReadableContainer import NotFoundException, ReadableContainer
 from .Container import Container, NotExtendableException
 from .MultiContainer import MultiContainer
+
+# TODO Annotations for registering and injecting services
