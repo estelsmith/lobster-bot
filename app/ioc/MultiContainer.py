3a5f24849f34446e9c0bece794595657cac64b2a
Estel Smith <estel.smith@gmail.com>
2025-07-06 16:38:53 -0400

    MultiContainer.attach()

--- app/ioc/MultiContainer.py
+++ app/ioc/MultiContainer.py
@@ -2,15 +2,26 @@ from typing import Optional
 
 from app.ioc import Container, NotFoundException, ReadableContainer
 
+class ContainerAlreadyAddedException(Exception):
+    """Exception thrown when the container being attached already exists."""
+    pass
+
 class MultiContainer(ReadableContainer):
     """
     Pull from multiple DI containers instead of just a single one, allowing for composing more complex containers from a
     collection of simpler ones
     """
 
-    def __init__(self, containers: Optional[list[Container]]):
+    def __init__(self, containers: Optional[list[Container]] = None):
         super().__init__()
         self._containers: list[Container] = containers if containers else []
+
+    def attach(self, container: Container):
+        """Attach container to instance, allowing it to be referenced when resolving services."""
+        if container in self._containers:
+            raise Exception('Container already attached')
+
+        self._containers.append(container)
 
     def raw(self, name):
         """
